18348c7186bb63d64461e6ad8cf9d033704ed6d7
ZeErdada <1371598840@qq.com>
2017-09-11 09:37:33 +0800

    使用 ExpandableListView +Checkbox实现购物车功能

---
+++ app/src/main/java/com/example/administrator/wangye2017_9_11/Bean/CartBean.java
@@ -0,0 +1,16 @@
+package com.example.administrator.wangye2017_9_11.Bean;
+
+/**
+ * Created by Administrator on 2017/9/11.
+ * 店铺和商品IDBean
+ * 王野
+ */
+
+public class CartBean {
+
+    /** 商品ID */
+    private String goodsid;
+    /** 店铺ID */
+    private String storeid;
+
+}
